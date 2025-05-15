cb4e1cd140335ad76ef4e595cd511540d106e13a
apyura <prlxndr@ua.fm>
2025-05-15 19:58:02 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿namespace _20250515_DelegatesDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Standard delegates
+
+            double[] source = new double[] { -2.4, 1.5, -8.1 };
+
+            Array.ForEach(source, Do);
+
+            #endregion
+
+
+            Publisher p1 = new Publisher(4);
+
+            p1.Run();
+
+            Console.WriteLine("------------------------------------------");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            #region Subscribe with static methods
+
+            //p1.Subscribe(DoHandler1);
+            p1.Started += DoHandler1;
+
+            #endregion
+
+            p1.Run();
+
+            Console.WriteLine("------------------------------------------");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            #region Subscribe with static methods
+
+            //p1.Subscribe(DoHandler2);
+            p1.Started += DoHandler2;
+
+            #endregion
+
+            p1.Run();
+
+            Console.WriteLine("------------------------------------------");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            #region UnSubscribe with static methods
+
+            //p1.UnSubscribe(DoHandler1);
+            p1.Started -= DoHandler1;
+
+            #endregion
+
+            p1.Run();
+        }
+
+        private static void DoHandler1(int iterationNumber)
+        {
+            ConsoleColor oldColor = Console.ForegroundColor;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"\tDoHandler1({iterationNumber})");
+
+            Console.ForegroundColor = oldColor;
+        }
+
+        private static void DoHandler2(int iterationNumber)
+        {
+            ConsoleColor oldColor = Console.ForegroundColor;
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine($"\t\tDoHandler2({iterationNumber})");
+
+            Console.ForegroundColor = oldColor;
+        }
+
+        public static void Do(double item)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
---
+++ Publisher.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20250515_DelegatesDemo
+{
+    public class Publisher
+    {
+        private NewIterationStarted _iterationStarted;
+        private int _iterationCount;
+
+        public Publisher(int iterationCount)
+        {
+            _iterationCount = iterationCount;
+        }
+
+        #region Subscribe / unsubscribe
+
+        //public void Subscribe(NewIterationStarted handler)
+        //{
+        //    //_iterationStarted += handler;    // додавання обробника в ланцюжок викликів
+
+        //    _iterationStarted = (NewIterationStarted)Delegate.Combine(_iterationStarted, handler);
+        //}
+
+        //public void UnSubscribe(NewIterationStarted handler)
+        //{
+        //    _iterationStarted -= handler;    // видалення обробника з ланцюжкя викликів
+        //}
+
+        #endregion
+
+        #region Work with event
+
+        //public event NewIterationStarted Started;
+        public event NewIterationStarted Started
+        {
+            add    // Subscribe
+            {
+                // Validation
+                _iterationStarted += value;
+            }
+            remove  // unsubscribe
+            {
+                // Validation
+                _iterationStarted -= value;
+            }
+        }
+
+        #endregion
+
+
+        public void Run()
+        {
+            for (int i = 0; i < _iterationCount; i++)
+            {
+                if (_iterationStarted != null)
+                {
+                    _iterationStarted(i);    // Call callback functions
+                }
+
+                Console.WriteLine($"iteration: {i}");
+            }
+        }
+    }
+}
